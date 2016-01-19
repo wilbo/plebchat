cdd854c826a06b8d960ad8c1dea48644aae1cb2a
Wilbert Schepenaar <wilbertschepenaar@hotmail.com>
2016-01-19 13:28:41 +0100

    added unique username and no symbols in username

--- app.js
+++ app.js
@@ -28,18 +28,29 @@ var users = {};
 io.on('connection', function(socket) {
 
 	// join request
-  socket.on('join', function(username) {
+  socket.on('join', function(name) {
+  	console.log('join request fired!');
 
-  	if (username == '') {
-  		io.emit('usernameEmpty');
-  	} else if (objectHasValue(users, username)) {
-  		io.emit('usernameTaken', username);
+  	if (name == '') {
+  		// if no name was provided
+  		io.emit('usernameError', 'please submit a username');
+  		//return false;
+  	} else if (containsSymbol(name)) {
+  		// if name contains wrong characters
+  		io.emit('usernameError', 'Your username may only contain letters and numbers.');
+  		//return false;
+  	} else if (usernameTaken(users, name)) {
+  		// if name already taken
+  		io.emit('usernameError', 'username is already taken.');
+  		//return false;
   	} else {
-  		users[socket.id] = username;
-    	io.emit('joined', username);
+  		// of everything is cool
+  		users[socket.id] = name;
+    	io.emit('joined', name);
+    	io.emit("updateUserList", users);
   	}
-
-  	console.log(users);
+  	
+  	//console.log(users);
    
   });
 
@@ -62,7 +73,12 @@ io.on('connection', function(socket) {
   // disconnecting
   socket.on('disconnect', function() {
   	var currentUser = users[socket.id];
-    io.emit('left', currentUser);
+  	// only if it was a joined user
+  	if (currentUser) {
+  		io.emit('left', currentUser);
+	    delete users[socket.id];
+	    io.emit("updateUserList", users);
+  	}
   });
 
 
@@ -73,10 +89,25 @@ io.on('connection', function(socket) {
 	helper functions
 */
 
-function objectHasValue(obj, val) {
+// check if the a string has symbols
+function containsSymbol(val) {
+	var charArray = val.toLowerCase().split('');
+	var allowedCharacters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+
+	for (var i=0; i < charArray.length; i++) {
+			if (allowedCharacters.indexOf(charArray[i]) == -1) {
+				return true;
+			}
+	}
+
+	return false;
+}
+
+// check if a value is inside an ibject
+function usernameTaken(obj, val) {
 	for(var prop in obj) {
     if(obj.hasOwnProperty(prop)) {
-      if(obj[prop] === val) {
+      if(obj[prop].toLowerCase() === val.toLowerCase()) {
           return true;
       }
     }
@@ -84,12 +115,23 @@ function objectHasValue(obj, val) {
 	return false;
 }
 
+
+
+
+
 function logArray(array) {
 	console.log('in array:');
 	for(var i = 0; i < array.length; i++) {
 		console.log(i + ': ' + array[i]);
 	}
 }
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next();
+});
 
 http.listen(3000, function() {
   console.log('listening on *:3000');
--- public/js/globals.js
+++ public/js/globals.js
@@ -1,51 +1,24 @@
 
 $(document).ready(function() {
 
-  /*
-      Socket.io stuff
-  */
 
   var socket = io();
 
   // focus automagically
   $('#name-input').focus();
 
+  /*
+    Requests
+  */
+
   // request to join
-  $('#name-form').submit(function() {
-    socket.emit('join', $('#name-input').val());
+  $('#name-form button').click(function() {
+    var username = $('#name-input').val();
+    socket.emit('join', username);
     return false;
   });
 
-  // empty username field
-  socket.on('usernameEmpty', function() {
-      output = "Fill in a user name.";
-      $('#name-form').append(output);
-  });
-
-
-  socket.on('usernameTaken', function() {
-      output = "This username us already taken.";
-      $('#name-form').append(output);
-  });
-
-  // when connected
-  socket.on('joined', function(usr) {
-    $('.overlay').fadeOut(200).remove();
-    $('#message-input').focus();
-    var output = '<div class="message-box"><div class="message">' + usr + ' joined.</div></div>';
-    $('#messages').append(output);
-  });
-
-  // socket.on('connectMessage', function(socketName) {
-  //   var message = '<div class="message-box"><div class="message">' + socketName + ' connected</div>';
-  //   $('#messages').append(message);
-  // })
-
-  // socket.on('nameError', function(socketName) {
-  //   var message = 'sorry, that name is already taken. Choose another.';
-  //   $('#name-form').append(message);
-  // });
-
+  // request to post a message
   $('#message-form').submit(function() {
     var message = $('#message-input').val();
     socket.emit('messageRequest', message);
@@ -53,32 +26,47 @@ $(document).ready(function() {
     return false;
   });
 
+  /*
+    Responses
+  */
+
+  // username error
+  socket.on('usernameError', function(errorMessage) {
+      $('#name-error').empty();
+      $('#name-error').append(errorMessage);
+  });
+
+  // when connected
+  socket.on('joined', function(username) {
+    $('.overlay').fadeOut(200).remove();
+    $('#message-input').focus();
+    var output = '<div class="message-box"><div class="message">' + username + ' joined.</div></div>';
+    $('#messages').append(output);
+  });
+
+  // append message
   socket.on('messageResponse', function(data) {
     output = '<div class="message-box"><div class="name">' + data.username + '</div><div class="time">' + data.timestamp + '</div><div class="message">' + data.message + '</div>';
     $('#messages').append(output);
   });
 
+  // update userlist
+  socket.on('updateUserList', function(users) {
+    $("#user-list").empty();
+    $.each(users, function(id, username) {
+        $('#user-list').append('<li id="' + username + '">' + username + '</li>');
+    });
+  });
+
   // when disconnectes
-  socket.on('left', function(usr) {
-    output = '<div class="message-box"><div class="message">' + usr + ' has left.</div></div>';
+  socket.on('left', function(username) {
+    output = '<div class="message-box"><div class="message">' + username + ' has left.</div></div>';
     $('#messages').append(output);
   });
 
   /*
       Other stuff
   */
-
-  function constructMessage(nm, ts, msg) {
-    output = '';
-    output += '<div class="message-box"><div class="name">';
-    output += nm;
-    output += '</div><div class="time">';
-    output += ts;
-    output += '</div><div class="message">';
-    output += msg;
-    output += '</div>';
-    return output;
-  }
 
 
 });
